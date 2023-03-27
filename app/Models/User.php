d3631d1cd1d996ce3d93989bd33acb4896f62b29
Kiki Adhani <81797183+kiki-adhani@users.noreply.github.com>
2023-03-27 13:01:52 +0700

    Add files via upload

---
+++ app/Models/User.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Jetstream\HasTeams;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class User extends Authenticatable
+{
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use HasTeams;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
+    use SoftDeletes;
+
+    protected $dates = [
+        'updated_at',
+        'created_at',
+        'deleted_at',
+        'email_verified_at',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string<int, string>
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array<int, string>
+     */
+    protected $appends = [
+        'profile_photo_url',
+    ];
+
+    // one to one
+    public function detail_user()
+    {
+        return $this->hasOne('App\Models\DetailUser', 'users_id');
+    }
+
+    // one to many
+    public function service()
+    {
+        return $this->hasMany('App\Models\Service', 'users_id');
+    }
+    // ini yg atas users_id atau service_id ya
+
+    public function order_buyer()
+    {
+        return $this->hasMany('App\Models\Order', 'buyer_id');
+    }
+
+    public function freelancer()
+    {
+        return $this->hasMany('App\Models\Order', 'freelancer_id');
+    }
+}
